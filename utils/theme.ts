24885f023912df4a521ce89ee55d37fd9f8383a8
minhnv <nvminh.hust@gmail.com>
2023-09-04 22:15:16 +0700

    setup base

---
+++ utils/theme.ts
@@ -0,0 +1,15 @@
+import {createTheme} from '@mui/material/styles';
+
+// Create a theme instance
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF6464',
+    },
+    secondary: {
+      main: '#00A8CC',
+    },
+  },
+});
+
+export default theme;
