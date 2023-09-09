e3b3b1085d78be89761c61cb60a1dd410038ea92
Minhnv <minh.ngo1@onemount.com>
2023-09-09 16:19:32 +0700

    init base

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,36 @@
-import '../styles/globals.css';
-import type {AppProps} from 'next/app';
+import {CacheProvider} from '@emotion/react'
+import CssBaseline from '@mui/material/CssBaseline'
+import {ThemeProvider} from '@mui/material/styles'
+import {SWRConfig} from 'swr'
+import '../styles/globals.css'
+import {createEmotionCache} from "@utils/create-emotion-cache";
+import {AppPropsWithLayout} from "@models/common";
+import {theme} from "@utils/theme";
+import axiosClient from "../api-client/axios-client";
 
-function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({
+                   Component,
+                   pageProps,
+                   emotionCache = clientSideEmotionCache,
+               }: AppPropsWithLayout) {
+    const Layout = Component.Layout
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+
+                <SWRConfig value={{fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false}}>
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </SWRConfig>
+            </ThemeProvider>
+        </CacheProvider>
+    )
 }
-export default MyApp;
+
+export default MyApp
